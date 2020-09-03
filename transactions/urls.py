e65f991f742b7df237d5c027719b05eb0dbf179d
Doug Penny <penny@nrcaknights.com>
2020-09-02 21:16:59 -0400

    WIP: Making progress on the various admin views

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
 
-from transactions.views import TransactionCreateView, TransactionDetailView, TransactionListView, TransactionDepositView
+from transactions.views import TransactionCreateView, TransactionsDateArchiveView, TransactionDetailView, TransactionListView, TransactionDepositView, TransactionsTodayArchiveView, TransactionProcessView
 
 
 urlpatterns = [
     path('', TransactionListView.as_view(), name='transaction-list'),
+    path('<int:year>/<int:month>/<int:day>/', TransactionsDateArchiveView.as_view(month_format='%m'), name='transaction-date-list'),
+    path('today/', TransactionsTodayArchiveView.as_view(), name='transaction-today-list'),
+
+    path('deposits/', TransactionListView.as_view(filter='deposits'), name='transaction-deposits'),
+    path('deposits/<int:year>/<int:month>/<int:day>/', TransactionsDateArchiveView.as_view(filter='deposits', month_format='%m'), name='transaction-date-deposits'),
+    path('deposits/new/', TransactionDepositView.as_view(), name='transaction-deposit-create'),
+    path('deposits/today/', TransactionsTodayArchiveView.as_view(filter='deposits'), name='transaction-today-deposits'),
+
+    path('orders/', TransactionListView.as_view(filter='orders'), name='transaction-orders'),
+    path('orders/<int:year>/<int:month>/<int:day>/', TransactionsDateArchiveView.as_view(filter='orders', month_format='%m'), name='transaction-date-orders'),
+    path('orders/today/', TransactionsTodayArchiveView.as_view(filter='orders'), name='transaction-today-orders'),
+    path('orders/new/', TransactionDepositView.as_view(), name='transaction-order-create'),
+    path('orders/process/<int:year>/<int:month>/<int:day>/', TransactionProcessView.as_view(), name='transaction-date-process'),
+    path('orders/process/<int:pk>/', TransactionProcessView.as_view(), name='transaction-single-process'),
+
     path('create/', TransactionCreateView.as_view(), name='transaction-create'),
     path('<int:pk>/', TransactionDetailView.as_view(), name='transaction-detail'),
-    path('deposit/', TransactionDepositView.as_view(), name='transaction-deposit'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -1,8 +1,31 @@
 
-from django.views.generic import CreateView, ListView, DetailView, FormView
+from django.http import HttpResponse
+from django.utils import timezone
+from django.views import View
+from django.views.generic import CreateView, ListView, DetailView, FormView, DayArchiveView, TodayArchiveView
 
 from .models import Transaction
 from .forms import TransactionForm, TransactionDepositForm
+
+
+class TransactionMixin:
+    filter = None
+    allow_empty = True
+    date_field = "submitted"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = self.filter
+        context['today'] = timezone.now
+        return context
+
+    def get_queryset(self):
+        if self.filter == 'deposits':
+            return Transaction.objects.filter(transaction_type=Transaction.CREDIT)
+        elif self.filter == 'orders':
+            return Transaction.objects.filter(transaction_type=Transaction.DEBIT)
+        else:
+            return Transaction.objects.all()
 
 
 class TransactionCreateView(CreateView):
@@ -11,9 +34,18 @@ class TransactionCreateView(CreateView):
     template_name = 'transactions/transaction_create.html'
     queryset = Transaction.objects.all()
 
-class TransactionListView(ListView):
+
+class TransactionListView(TransactionMixin, ListView):
     template_name = 'transactions/transactions_list.html'
-    queryset = Transaction.objects.all()
+
+
+class TransactionsDateArchiveView(TransactionMixin, DayArchiveView):
+    template_name = 'transactions/transactions_list.html'
+
+
+class TransactionsTodayArchiveView(TransactionMixin, TodayArchiveView):
+    template_name = 'transactions/transactions_list.html'
+
 
 class TransactionDetailView(DetailView):
     template_name = 'transactions/transaction_detail.html'
@@ -30,3 +62,16 @@ class TransactionDepositView(FormView):
         print('Check #: {}'.format(form.cleaned_data['check_num']))
         print('Amount: {}'.format(form.cleaned_data['amount']))
         return super().form_valid(form)
+
+
+class TransactionProcessView(View):
+    def get(self, request, *args, **kwargs):
+        if self.kwargs['year']:
+            print('Year: {}'.format(self.kwargs['year']))
+        if self.kwargs['month']:
+            print('Year: {}'.format(self.kwargs['month']))
+        if self.kwargs['day']:
+            print('Year: {}'.format(self.kwargs['day']))
+        if self.kwargs['pk']:
+            print('Year: {}'.format(self.kwargs['pk']))
+        return HttpResponse('Hello, World!')
