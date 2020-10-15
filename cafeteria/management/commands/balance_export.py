f7f91cf075d20736f44bef6cc1444c82ac37340b
Doug Penny <penny@nrcaknights.com>
2020-10-15 16:20:47 -0400

    Remove the field names from balance export file

--- cafeteria/management/commands/balance_export.py
+++ cafeteria/management/commands/balance_export.py
@@ -17,11 +17,9 @@ class Command(BaseCommand):
         logger.info('Exporting lunch balances...')
         file_path = os.getenv('BALANCE_EXPORT_PATH')
         students = Profile.objects.filter(role=Profile.STUDENT)
-        fields = ['student_number', 'balance1']
         filename = os.path.join(file_path, 'lunch_balance.csv')
         with open(filename, 'w') as csvfile:
             csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(fields)
             for student in students:
                 csvwriter.writerow([student.user_number, student.current_balance])
         logger.info('Finished exporting lunch balances.')
