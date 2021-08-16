dd224c83e6ef230fd86e8a743de500a482b6580e
Doug Penny <penny@nrcaknights.com>
2021-08-15 21:20:03 -0400

    Another correction for API permissions

--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 from rest_framework import filters
 from rest_framework import generics
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
