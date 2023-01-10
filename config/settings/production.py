71421d9c83ea4dd6629ebf8ed6cc4dfefe2c5107
Doug Penny <penny@nrcaknights.com>
2023-01-09 22:25:56 -0500

    Tweak production database settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,27 +6,26 @@ import sys
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
-if len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-    if os.getenv('POSTGRES_HOST') is None:
-        raise Exception('POSTGRES_HOST environment variable not defined')
-    if os.getenv('POSTGRES_NAME') is None:
-        raise Exception('POSTGRES_NAME environment variable not defined')
-    if os.getenv('POSTGRES_USER') is None:
-        raise Exception('POSTGRES_USER environment variable not defined')
-    if os.getenv('POSTGRES_PASSWORD') is None:
-        raise Exception('POSTGRES_PASSWORD environment variable not defined')
-    if os.getenv('POSTGRES_PORT') is None:
-        raise Exception('POSTGRES_PORT environment variable not defined')
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('POSTGRES_NAME'),
-            'USER': os.getenv('POSTGRES_USER'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-            'HOST': os.getenv('POSTGRES_HOST'),
-            'PORT': os.getenv('POSTGRES_PORT'),
-        }
+if os.getenv('POSTGRES_HOST') is None:
+    raise Exception('POSTGRES_HOST environment variable not defined')
+if os.getenv('POSTGRES_NAME') is None:
+    raise Exception('POSTGRES_NAME environment variable not defined')
+if os.getenv('POSTGRES_USER') is None:
+    raise Exception('POSTGRES_USER environment variable not defined')
+if os.getenv('POSTGRES_PASSWORD') is None:
+    raise Exception('POSTGRES_PASSWORD environment variable not defined')
+if os.getenv('POSTGRES_PORT') is None:
+    raise Exception('POSTGRES_PORT environment variable not defined')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
+}
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
